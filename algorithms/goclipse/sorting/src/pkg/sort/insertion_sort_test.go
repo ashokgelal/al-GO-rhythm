13d67aa1bd5e0ec401ea4669fdf20e6a8ce522d1
Ashok Gelal <ashokgelal@gmail.com>
2011-10-09 23:47:25 -0600

    more tests

--- algorithms/goclipse/sorting/src/pkg/sort/insertion_sort_test.go
+++ algorithms/goclipse/sorting/src/pkg/sort/insertion_sort_test.go
@@ -5,8 +5,7 @@ import (
 	"fmt"
 )
 
-
-func TestSort(t *testing.T){
+func TestSortDesc(t *testing.T){
 	t1 := &(MyType{3})
 	t2 := &(MyType{2})
 	t3 := &(MyType{4})
@@ -17,8 +16,31 @@ func TestSort(t *testing.T){
 	v := []MyType{*t3, *t2, *t4, *t1, *t5, *t6, *t7}
 	
 	fmt.Printf("Before sorting: %v\n", v)
-	Sort(&v, compare)
-	fmt.Printf("After sorting: %v\n", v)
+	Sort(&v, compare_desc)
+	fmt.Printf("After sorting in desc. order: %v\n", v)
+	
+	for i:= 0; i<len(v)-1; i++{
+		x := v[i].data.(int)
+		if y := v[i+1].data.(int); x < y{
+			t.Errorf("%d > %d", x, y)
+			return
+		}
+	}
+}
+
+func TestSortAsc(t *testing.T){
+	t1 := &(MyType{3})
+	t2 := &(MyType{2})
+	t3 := &(MyType{4})
+	t4 := &(MyType{1})
+	t5 := &(MyType{8})
+	t6 := &(MyType{5})
+	t7 := &(MyType{6})
+	v := []MyType{*t3, *t2, *t4, *t1, *t5, *t6, *t7}
+	
+	fmt.Printf("Before sorting: %v\n", v)
+	Sort(&v, compare_asc)
+	fmt.Printf("After sorting in asc. order: %v\n", v)
 	
 	for i:= 0; i<len(v)-1; i++{
 		x := v[i].data.(int)
@@ -29,7 +51,11 @@ func TestSort(t *testing.T){
 	}
 }
 
-func compare(a MyType, b MyType) bool {
+func compare_asc(a MyType, b MyType) bool {
 	return a.data.(int) < b.data.(int)
+}
+
+func compare_desc(a MyType, b MyType) bool {
+	return a.data.(int) > b.data.(int)
 }
 
